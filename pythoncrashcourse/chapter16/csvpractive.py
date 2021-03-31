ba03dba5bfa75838ac3ae9d257849ccbb5882776
mariloca <58670289+mariloca@users.noreply.github.com>
2021-03-31 16:28:58 -0700

    add low temps and shading

--- pythoncrashcourse/chapter16/csvpractive.py
+++ pythoncrashcourse/chapter16/csvpractive.py
@@ -9,7 +9,10 @@ datetime module
 strptime(): interpret the date. Convert a string to a datetime format. 
 			Second argument tells Python how the date is formatted.
 '''
-filename='sitka_weather_07-2014.csv'
+#filename='sitka_weather_07-2014.csv'
+#filename='/Users/MarisolWang/Documents/GitHub/pcc/chapter_16/sitka_weather_2014.csv'
+filename='/Users/MarisolWang/Documents/GitHub/pcc/chapter_16/death_valley_2014.csv'
+
 with open(filename) as f:
 	reader=csv.reader(f)
 	header_row=next(reader)
@@ -24,27 +27,36 @@ with open(filename) as f:
 
 
 	'''
-	Get dates and high temperatures from file.
+	Get dates, high, and low temperatures from file.
 
 	The reader object continues from where it left off in the CSV file
 	and automatically returns each line following its current position.
 	next()has already read the header row,
 	so the loop will begin at the second line where the actual data begins.
 	'''
-	dates,highs=[],[]
+	dates,highs,lows=[],[],[]
 	for row in reader:
-		current_date=datetime.strptime(row[0],"%Y-%m-%d")
-		dates.append(current_date)
-
-		high=int(row[1])
-		highs.append(high)
+		try:
+			current_date=datetime.strptime(row[0],"%Y-%m-%d")
+			high=int(row[1])
+			low=int(row[3])
+		except ValueError:
+			print(current_date,'missing data')
+		else:
+			dates.append(current_date)
+			highs.append(high)
+			lows.append(low)
 
 #plot data
 fig=plt.figure(dpi=128,figsize=(10,6))
-plt.plot(dates,highs,c='red') #plot the points in red
+plt.plot(dates,highs,c='red',alpha=0.5) #plot the points in red
+#'alpha' controls a color's transparency, alpha=0: complete transparent, alpha=1(default)is complete opaque.
+plt.plot(dates,lows,c='blue',alpha=0.5)
+#fill_between(): takes a series of x-values and two series of y-values and fills the space between the two y-value series.
+plt.fill_between(dates, highs, lows, facecolor='blue',alpha=0.1)
 
 #format plot
-plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.title("Daily high and low temperatures - 2014\nDeath Valley, CA", fontsize=20)
 plt.xlabel('',fontsize=16)
 fig.autofmt_xdate() #draws the date labels diagonally to prevent them from overlapping.
 plt.ylabel("Temperature(F)", fontsize=16)
