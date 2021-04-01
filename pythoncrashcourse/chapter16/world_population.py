f7936f977426c0c103ffe05670425153a3966afd
mariloca <58670289+mariloca@users.noreply.github.com>
2021-03-31 22:35:55 -0700

    plot a complete population map

--- pythoncrashcourse/chapter16/world_population.py
+++ pythoncrashcourse/chapter16/world_population.py
@@ -7,6 +7,8 @@ filename='/Users/MarisolWang/Documents/GitHub/pcc/chapter_16/population_data.jso
 with open(filename) as f:
 	pop_data=json.load(f) #converts the json data into a list
 
+#Build a dictionary of population data
+cc_populations={}
 #Print the 2010 population for each country
 for pop_dict in pop_data:
 	if pop_dict['Year']=='2010':
@@ -14,6 +16,9 @@ for pop_dict in pop_data:
 		population=int(float(pop_dict['Value']))
 		code=get_country_code(country_name)
 		if code:
-			print(code+": "+str(population))
-		else:
-			print("Error - " + country_name)
+			cc_populations[code]=population
+
+wm=py.World()
+wm.title="World Population in 2010 by Country"
+wm.add('2010', cc_populations)
+wm.render_to_file("world_population.svg")
