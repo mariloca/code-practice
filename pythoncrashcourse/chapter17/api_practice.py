1bd4a482df01917fc126e9ac1d0054f516cdc5ca
mariloca <58670289+mariloca@users.noreply.github.com>
2021-04-08 18:31:16 -0700

    add tooltip

--- pythoncrashcourse/chapter17/api_practice.py
+++ pythoncrashcourse/chapter17/api_practice.py
@@ -15,10 +15,17 @@ print("Total repositories: ", response_dict['total_count'])
 repo_dicts=response_dict['items']
 #print("Repositories returned:", len(repo_dicts))
 
-names,stars=[],[]
+names,plot_dicts=[],[]
 for repo_dict in repo_dicts:
 	names.append(repo_dict['name'])
-	stars.append(repo_dict['stargazers_count'])
+	#stars.append(repo_dict['stargazers_count'])
+
+	#Get the project description, if one is available
+	description=repo_dict['description']
+	if not description:
+		description="No description provided."
+	plot_dict = {'value': repo_dict['stargazers_count'], 'label':description,} #value to determine bar height, label to create tooltip for each bar.
+	plot_dicts.append(plot_dict)
 
 #Make visualization
 my_style=LS('#333366', base_style=LCS)
@@ -37,7 +44,7 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title='Most-starred Python Projects on GitHub'
 chart.x_labels=names
 
-chart.add('',stars) #height of the bar. Don't need to be labeled, so pass an empty string
+chart.add('',plot_dicts) #height of the bar. Don't need to be labeled, so pass an empty string
 chart.render_to_file('python_repos.svg')
 
 '''
