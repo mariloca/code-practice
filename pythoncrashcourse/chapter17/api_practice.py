ae726c1eb5224789d6a5e4f4ecedde64181ad3cf
mariloca <58670289+mariloca@users.noreply.github.com>
2021-04-08 17:25:59 -0700

    working with the Response Dict

--- pythoncrashcourse/chapter17/api_practice.py
+++ pythoncrashcourse/chapter17/api_practice.py
@@ -3,10 +3,28 @@ import requests
 #Make an API call and store the response.
 url='https://api.github.com/search/repositories?q=language:python&sort=stars'
 r=requests.get(url)
-print("Status Code:", r.status_code)
+print("Status Code:", r.status_code) #code 200 indicates a successful response
 
 #Store API response in a variable
-response_dict=r.json()
+response_dict=r.json() #json() converts information to a dict
+print("Total repositories: ", response_dict['total_count'])
 
+#Explore information about the repositories
+repo_dicts=response_dict['items']
+print("Repositories returned:", len(repo_dicts))
+
+#Examine the first repositories
+repo_dict=repo_dicts[0]
+#print("\nKeys:", len(repo_dict))
+#for key in sorted(repo_dict.keys()):
+#	print(key)
+print("\nSelected information about first respository:")
+print("Name:", repo_dict['name'])
+print("Owner:", repo_dict['owner']['login'])
+print("Stars:", repo_dict['stargazers_count'])
+print("Repository:", repo_dict['html_url'])
+print("Created:", repo_dict['created_at'])
+print("Updated:", repo_dict['updated_at'])
+print("Description:", repo_dict['description'])
 #Process results.
-print(response_dict.keys())
+#print(response_dict.keys())
